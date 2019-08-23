3938447e7c63c4efa57c558146f67064877b532f
Carlos Vinicius <45642896+CarlosVini2512@users.noreply.github.com>
2019-08-23 01:44:42 -0400

    Add files via upload

---
+++ SistemaDesafio/src/br/com/hospitalif/controller/Administrador.java
@@ -0,0 +1,18 @@
+package br.com.hospitalif.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Administrador {
+
+    @FXML
+    private Button buttonCadastrar;
+
+    @FXML
+    void CadastrarFuncionario(ActionEvent event) {
+
+    }
+
+}
+
