a117ff600bd9e054803b04385ae7543a10358d8a
45244451880 <45244451880@192.168.56.1>
2019-11-08 22:41:08 -0400

    08/11

--- hospitalfx.sql
+++ hospitalfx.sql
@@ -8,14 +8,21 @@ sexo varchar(50),
 statusPessoa varchar(200), 
 login varchar(50),
 senha varchar(50),
-statusUsuario varchar(200),
 numeroRegistro int,
 especialidade varchar(50),
 constraint PK_Medico primary key(idMedico));
 
 create table Enfermeiro (
 id int not null AUTO_INCREMENT,
+nome varchar(50),
+cpf varchar(50),
+idade int,
+tipoSanguineo varchar(50) ,
+sexo varchar(50),
+login varchar(50),
+senha varchar(50),
 numeroRegistro varchar(50),
+especialidade varchar(50),
 constraint PK_Enfermeiro primary key(idEnfermeiro));
 
 create table Gerente (
@@ -66,12 +73,3 @@ dData date,
 doenca varchar(300),
 constraint PK_Atendimento primary key (idAtendimento));
 
-create table Pessoa (
-idPessoa int not null AUTO_INCREMENT,
-nome varchar(60),
-cpf varchar(12),
-idade int ,
-tipoSanguineo varchar(3),
-sexo varchar(10),
-statusPessoa varchar(150), 
-constraint PK_Medico primary key(idMedico));
